0bc5ff9f45b552274c38f08d592b41fcdb2a9fc4
Charlie Lehman <charlie.k.lehman@gmail.com>
2017-02-09 13:54:38 -0500

    test

--- kds17_pre.py
+++ kds17_pre.py
@@ -187,7 +187,8 @@ class DicomImage:
         An object containing:
 
         DicomImage.scan: np.array of scan
-        DicomImage.spacing: 3x1 array of spacing adjustment on image to normalize to 1mm^3
+        DicomImage.spacing: 3x1 array of spacing adjustment on 
+                            image to normalize to 1mm^3
         DicomImage.<Args>
 
     '''
@@ -218,6 +219,59 @@ class DicomImage:
         ax.set_zlim(0, p.shape[2])
 
         plt.show()
+    def __generate_mask(self):
+        '''generate mask'''
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)
+
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]
+
+    if len(counts) > 0:
+        return vals[np.argmax(counts)]
+    else:
+        return None
+
+def segment_lung_mask(image, fill_lung_structures=True):
+
+# not actually binary, but 1 and 2. 
+# 0 is treated as background, which we do not want
+    binary_image = np.array(image > -320, dtype=np.int8)+1
+    labels = measure.label(binary_image)
+
+# Pick the pixel in the very corner to determine which label is air.
+#   Improvement: Pick multiple background labels from around the patient
+#   More resistant to "trays" on which the patient lays cutting the air 
+#   around the person in half
+    background_label = labels[0,0,0]
+
+#Fill the air around the person
+    binary_image[background_label == labels] = 2
+
+
+# Method of filling the lung structures (that is superior to something like 
+# morphological closing)
+    if fill_lung_structures:
+# For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
+
+            if l_max is not None: #This slice contains some lung
+                binary_image[i][labeling != l_max] = 1
+
+
+    binary_image -= 1 #Make the image actual binary
+    binary_image = 1-binary_image # Invert it, lungs are now 1
+
+# Remove other air pockets insided body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+    if l_max is not None: # There are air pockets
+        binary_image[labels != l_max] = 0
+
+    return binary_image
 
     def __rescale(self, slices):
         image = np.stack([s.pixel_array for s in slices])
@@ -315,7 +369,8 @@ class DicomBatch:
             print('Interrupt Caught. Terminating now...')
 
     def __mask(self, im):
-        '''do something with masking'''
+        assert 'zoom' in self.process_list, 'You need to apply zoom via process_batch' 
+        assert 'mask' not in self.process_list, 'You don\'t need to mask this batch you have one.'
         
     def __resample(self, im):
         resize_factor = im.spacing / self.spacing
@@ -333,8 +388,6 @@ class DicomBatch:
 
 def main(argv=None):
     x = DicomDict(im_dir, label_dir)
-    print(x.total_size>>20)
-    print(x.dicom_dict)
 
     #y = DicomBatch(x, 'test_batch')
     #y.process_batch('zoom')
