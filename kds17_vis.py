cf588b73e7e56aadb3297c14aa68e2871ae85d2e
Charlie Lehman <charlie.k.lehman@gmail.com>
2017-02-17 12:57:48 -0500

    ADDED: Consolidated all visualization methods to kds17_vis.py

--- kds17_vis.py
+++ kds17_vis.py
@@ -16,8 +16,12 @@
 """Visualisation handling of DICOM Images for Kaggle Data Science 2017
 
 """
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation as an
 
 def animate(image):
+    print('Preparing animation')
     fig = plt.figure()
     ims = []
     ax = fig.add_subplot(111)
@@ -25,6 +29,7 @@ def animate(image):
         im = plt.imshow(image[i], cmap=plt.cm.gray)
         ims.append([im])
     ani = an.ArtistAnimation(fig, ims, interval=150, blit=True)
+    print('Animating')
     plt.show()
 
 def rotate(image, pos):
